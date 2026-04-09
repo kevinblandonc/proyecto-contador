b7e4c8710b6048da4535c5b09d4ee511dfd461ab
Tu Nombre <kevinblandonc@gmail.com>
2026-04-09 11:54:47 -0600

    Add reset button feature

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 let contador=0;
 const spanNumero = document.getElementById("numero");
 const btnAumentar = document.getElementById("btn-aumentar");
+const btnReset = document.getElementById("btn-reset");
 
 btnAumentar.addEventListener("click",()=>{
     contador++;
     spanNumero.innerText=contador;
+});
+
+btnReset.addEventListener('click', () => {
+    contador = 0;
+    spanNumero.innerText = contador;
 });
 
